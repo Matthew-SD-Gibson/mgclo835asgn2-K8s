cbdf1e93863632d02b3d40bd20bac3669ff6ab74
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-02-28 17:20:23 -0500

    Update init_kind.sh

--- K8s/init_kind.sh
+++ K8s/init_kind.sh
@@ -4,7 +4,6 @@
     sudo yum install docker -y
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
-    newgrp docker
     curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
     sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
     rm -f ./kind
